09a07788b05e5d8223267240cc6b6ad1bf60b655
jungnoeun <noeun1102@gmail.com>
2021-04-03 11:07:09 -0700

    yet

--- Q5.c
+++ Q5.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <time.h>
+#include <sys/stat.h>
 #include <string.h>
 
 
@@ -11,52 +12,62 @@ int main(){
 	int fd1,fd2;
 	int rcnt;
 	int ip=0;
-	char *fname1 = "bef_file.txt";
-	char *fname2 = "aft_file.txt";
+	char *fname1 = "bfile.txt";
+	char *fname2 = "afile.txt";
+	char *space = " ";
 	
 	printf("몇개의 난수를 입력하시겠습니까? : ");
 	scanf("%d",&num);
 	int arr1[1000] = {0,};
-	char str[1000]="";
-	char *wh = "";
+	char str[1000];
+	
 
 	srand(time(NULL));
 	for(int i=0;i<num;i++){
 		arr1[i] = rand()%100;
 	}
-	if((fd1 = open(fname1,O_RDWR|O_CREAT,0755))<0){
+	if((fd1 = open(fname1,O_WRONLY|O_CREAT,200))<0){
 		fprintf(stderr,"open error for %s\n",fname1);
 		exit(1);
 	}
-	if((fd2 = open(fname2,O_RDWR|O_CREAT,0755))<0){
-		fprintf(stderr,"open error for %s\n",fname2);
+
+
+	//for(int i=0;i<num;i++){
+	//	sprintf(str,"%s%d ",str, arr1[i]);
+	//}
+	
+	for(int i=0;i<num;i++){
+		sprintf(str,"%d",arr1[i]);
+		write(fd1,str,strlen(str));
+		write(fd1,space,1);
+	}
+
+	if(fchmod(fd1,S_IRWXU)<0){
+		printf("error");
+	}
+
+	//write(fd1,str,strlen(str));
+
+	close(fd1);
+
+
+	if((fd1 = open(fname1,O_RDONLY))<0){
+		fprintf(stderr,"open error for %s\n",fname1);
 		exit(1);
 	}
-
-
-	for(int i=0;i<num;i++){
-		sprintf(str,"%s%d ",str, arr1[i]);
-	}
 	
-	printf("정수배열을 문자열로 만든 후 %s\n",str);
-	write(fd1,str,strlen(str));
 
-	 
 	//딱 read로 fd1읽어들여야 함
-	read(fd1,wh,sizeof(wh));
+	read(fd1,str,1000);
 
-	wh = strtok(str," ");
+
+	char *wh = strtok(str," ");
 	while(wh!=NULL){
 		arr1[ip] = atoi(wh);
 		wh = strtok(NULL," ");
 		ip++;
 	}
 
-	printf("문자를 다시 정수배열로 만든 후\n");
-	for(int i=0;i<num;i++){
-		printf("%d ",arr1[i]);
-	}
-	printf("\n");
 	
 	for(int i=0;i<num-1;i++){
 		for(int j=0;j<num-i-1;j++){
@@ -69,22 +80,20 @@ int main(){
 	}
 
 	
-	printf("크기 순서대로 정렬한 후\n");
-	for(int i=0;i<num;i++){
-		printf("%d ",arr1[i]);
-	}
-	printf("\n");
-		
 	
-	for(int i=0;i<num;i++){
-		sprintf(str,"%s%d ",str,arr1[i]);
+	if((fd2 = open(fname2,O_RDWR|O_CREAT,200))<0){
+		fprintf(stderr, "open error for %s\n",fname2);
+		exit(1);
 	}
 	
-	printf("문자열로 최종 만든 후 %s\n",str);
-
-	write(fd2,str,strlen(str));
+	for(int i=0;i<num;i++){
+		sprintf(str,"%d",arr1[i]);
+		write(fd2,str,strlen(str));
+		write(fd2,space,1);
+	}
 	
 
+	//write(fd2,str,strlen(str));
 	
 	close(fd1);
 	close(fd2);
