aeb2e60001a03b2123756289c60aa92976fb4475
jungnoeun <noeun1102@gmail.com>
2021-04-15 23:22:38 -0700

    myls -l

--- myls.c
+++ myls.c
@@ -10,7 +10,9 @@
 #include <string.h>
 
 
-
+char type(mode_t);
+char *perm(mode_t);
+void printStat(char*, char*, struct stat*);
 int main(int argc, char *argv[]){
 
 	DIR *dp;
@@ -29,7 +31,7 @@ int main(int argc, char *argv[]){
 		while((d = readdir(dp)) != NULL){
 			cnt++;
 			printf("%-10s\t",d->d_name);
-			if(cnt%16 == 0)printf("\n");
+			if(cnt%8 == 0)printf("\n");
 		}
 		printf("\n");
 	}
@@ -51,12 +53,69 @@ int main(int argc, char *argv[]){
 					if(cnt%8==0)printf("\n");
 				}
 			}
+
+			if(strcmp(argv[1],"-l") == 0){
+				if((dp = opendir(dir)) == NULL)
+					perror(dir);
+				while((d = readdir(dp)) != NULL){
+					sprintf(path, "%s/%s",dir, d->d_name);
+					if(lstat(path,&st)<0)
+						perror(path);
+					printStat(path,d->d_name,&st);
+					//putchar('\n');
+				}
+			}
 		}
 	
 	}
 
 
-
+	closedir(dp);
 	exit(0);
 
 }
+
+//파일 상태 정보를 출력
+void printStat(char *pathname, char *file, struct stat *st){
+
+	//printf("%5ld ",st->st_blocks);
+	printf("%c%s ", type(st->st_mode),perm(st->st_mode));
+	printf("%3ld ",st->st_nlink);
+	printf("%s %s",getpwuid(st->st_uid)->pw_name, getgrgid(st->st_gid)->gr_name);
+	printf("%9ld ",st->st_size);
+	printf("%.12s",ctime(&st->st_mtime)+4);
+	printf("%s\n",file);
+}
+
+char type(mode_t mode){
+	if(S_ISREG(mode))
+		return('-');
+	if(S_ISDIR(mode))
+		return('d');
+	if(S_ISCHR(mode))
+		return('c');
+	if(S_ISBLK(mode))
+		return('b');
+	if(S_ISLNK(mode))
+		return('l');
+	if(S_ISFIFO(mode))
+		return('p');
+	if(S_ISSOCK(mode))
+		return('s');
+}
+
+
+char* perm(mode_t mode){
+	int i;
+	static char perms[10] = "---------";
+	for(i=0;i<3;i++){
+		if(mode&(S_IRUSR>>i*3))
+			perms[i*3] = 'r';
+		if(mode&(S_IWUSR>>i*3))
+			perms[i*3+1] = 'w';
+		if(mode&(S_IXUSR>>i*3))
+			perms[i*3+2] = 'x';
+	}
+	return(perms);
+}
+
